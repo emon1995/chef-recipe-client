4d850fb49238de9f19e45b954503b38a8894861b
emon1995 <emonhossain995@gmail.com>
2023-05-02 12:24:34 +0600

    Private route created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -68,7 +68,7 @@ const Header = () => {
         {user && (
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full" title={user?.displayName}>
-              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <img src={user?.photoURL} />
             </div>
           </label>
         )}
