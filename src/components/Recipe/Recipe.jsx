c6955aa7c176cb6ba1ffa27e8d2b7082bfcb7db0
emon1995 <emonhossain995@gmail.com>
2023-05-02 22:13:43 +0600

    favorite button added

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
+import { toast } from "react-hot-toast";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 
 const Recipe = ({ id }) => {
   const [recipes, setRecipes] = useState([]);
+  //   const [favorite, setFavorite] = useState(false);
+
+  const handleFavorite = (e) => {
+    e.target.setAttribute("disabled", "");
+    // console.log(e.target);
+    toast.success("Recipe is your Favorite");
+  };
 
   useEffect(() => {
     fetch(`http://localhost:5000/recipes/${id}`)
@@ -41,22 +50,12 @@ const Recipe = ({ id }) => {
               <p>{recipe?.rating}</p>
             </div>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary text-white gap-2">
+              <button
+                onClick={handleFavorite}
+                className="btn btn-primary cursor-pointer text-white gap-2"
+              >
                 Favorite
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
+                {/* <MdOutlineFavoriteBorder /> */}
               </button>
             </div>
           </div>
