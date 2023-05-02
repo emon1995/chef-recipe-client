b3b149db26f8cfde4b1b14eff17e7293051ba4e3
emon1995 <emonhossain995@gmail.com>
2023-05-02 12:42:26 +0600

    env file added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Spinner from "../Spinner/Spinner";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Spinner></Spinner>
     </div>
   );
 };
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDeJt3TP4iQi77ru4TmjGo91mf3bQlP1NU",
-  authDomain: "simple-firebase-react-acca5.firebaseapp.com",
-  projectId: "simple-firebase-react-acca5",
-  storageBucket: "simple-firebase-react-acca5.appspot.com",
-  messagingSenderId: "498450812220",
-  appId: "1:498450812220:web:0c9a2aecd7d9ff8a242239",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
